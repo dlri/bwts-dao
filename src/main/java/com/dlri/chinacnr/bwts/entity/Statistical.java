c60c9e7035dc1c6d468a9ad5da3116e7fbf6ace5
dlri <chenchuang.sy@163.com>
2018-10-07 23:11:59 +0800

    新环境后的修改

---
+++ src/main/java/com/dlri/chinacnr/bwts/entity/Statistical.java
@@ -0,0 +1,28 @@
+package com.dlri.chinacnr.bwts.entity;
+
+public class Statistical {
+	private String horizontalAxle ;
+	private float verticalAxleF;
+	private int  verticalAxleI;
+	public String getHorizontalAxle() {
+		return horizontalAxle;
+	}
+	public void setHorizontalAxle(String horizontalAxle) {
+		this.horizontalAxle = horizontalAxle;
+	}
+	public float getVerticalAxleF() {
+		return verticalAxleF;
+	}
+	public void setVerticalAxleF(float verticalAxleF) {
+		this.verticalAxleF = verticalAxleF;
+	}
+	public int getVerticalAxleI() {
+		return verticalAxleI;
+	}
+	public void setVerticalAxleI(int verticalAxleI) {
+		this.verticalAxleI = verticalAxleI;
+	}
+
+
+
+}
