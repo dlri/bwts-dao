6af16ff526ed546faf9faa49844e5bb3621391e9
dlri <chenchuang.sy@163.com>
2018-11-10 21:50:29 +0800

    2018年11月10日 V1.3

---
+++ src/main/java/com/dlri/chinacnr/bwts/entity/LoginInfo.java
@@ -0,0 +1,42 @@
+package com.dlri.chinacnr.bwts.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class LoginInfo implements Serializable {
+	private static final long serialVersionUID = 3528304610183292312L;
+	public String ip;
+	/**用户id*/
+	public Long uid;
+	/**登录时间*/
+	public Date loginTime=new Date();
+	public LoginInfo(){
+	}
+	public LoginInfo(String ip,Long uid,Date loginTime){
+		this.ip=ip;
+		this.uid=uid;
+		if(loginTime!=null)
+		this.loginTime=loginTime;
+	}
+	public static LoginInfo Init(){
+		return new LoginInfo();
+	}
+	public Long getUid() {
+		return uid;
+	}
+	public void setUid(Long uid) {
+		this.uid = uid;
+	}
+	public String getIp() {
+		return ip;
+	}
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+	public Date getLoginTime() {
+		return loginTime;
+	}
+	public void setLoginTime(Date loginTime) {
+		this.loginTime = loginTime;
+	}
+}
